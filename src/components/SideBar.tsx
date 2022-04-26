ad154c2f0739bae468bbe60b53dd1ac3cda5ba9a
Jośe Xavier <nettoxavier23@gmail.com>
2022-04-26 13:10:07 -0300

    challenge completed

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,27 @@
+
+import { UseMovies } from "../MoviesContext";
+import { Button } from "./Button";
+
 export function SideBar() {
   // Complete aqui
+  const {genres, handleClickButton, selectedGenreId} = UseMovies();
+
+  return (
+    <nav className="sidebar">
+    <span>Watch<p>Me</p></span>
+  
+    <div className="buttons-container">
+      {genres.map(genre => (
+        <Button
+          key={String(genre.id)}
+          title={genre.title}
+          iconName={genre.name}
+          onClick={() => handleClickButton(genre.id)}
+          selected={selectedGenreId === genre.id}
+        />
+      ))}
+    </div>
+  
+  </nav>
+  )
 }
